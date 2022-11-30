50990dfb60cb49531421ccc53f55ce4b703fd30a
daria_faskhieva <dashaf712@mail.ru>
2022-11-30 23:51:08 +0300

    личный кабинет

--- Фасхиева_ПР6/Pages/MenuAdmin.xaml.cs
+++ Фасхиева_ПР6/Pages/MenuAdmin.xaml.cs
@@ -25,35 +25,19 @@ namespace Фасхиева_ПР6
     {
         Clients user; // создаем объект для хранения информации о пользователе
 
-        void showImage(byte[] Barray, System.Windows.Controls.Image img)
-        {
-            BitmapImage BI = new BitmapImage();  // создаем объект для загрузки изображения
-            using (MemoryStream m = new MemoryStream(Barray))  // для считывания байтового потока
-            {
-                BI.BeginInit();  // начинаем считывание
-                BI.StreamSource = m;  // задаем источник потока
-                BI.CacheOption = BitmapCacheOption.OnLoad;  // переводим изображение
-                BI.EndInit();  // заканчиваем считывание
-            }
-            img.Source = BI;  // показываем картинку на экране (imUser – имя картиник в разметке)
-            img.Stretch = Stretch.Uniform;
-        }
-
         public MenuAdmin(Clients user)
         {
             InitializeComponent();
             this.user = user;
-            tbSurname.Text = tbSurname.Text + user.surname;
-            tbName.Text = tbName.Text + user.name;
-            tbPatronimyc.Text = tbPatronimyc.Text + user.patronimyc;
-            tbPhone.Text = tbPhone.Text + user.phone;
-            tbBirthday.Text = tbBirthday.Text + user.birthday.ToString("dd MMMM yyyy");
-
-            List<ClientPhoto> p = DataBase.bd.ClientPhoto.Where(x => x.idClient == user.idClient).ToList(); // для загрузки картинки находим все фото пользователя в таблице, где хранятся фото
-            if (p.Count != 0)  // если список с фото не пустой, начинает переводить байтовый массив в изображение
+            if (user.idRole == 2)
             {
-                byte[] Bar = p[p.Count - 1].photoBinary;   // считываем изображение из базы (считываем байтовый массив двоичных данных) - выбираем последнее добавленное изображение
-                showImage(Bar, imAdmin);  // отображаем картинку
+                btGroup.Visibility = Visibility.Collapsed;
+                btUsers.Visibility=Visibility.Collapsed;
+            }
+            else
+            {
+                btGroup.Visibility = Visibility.Visible;
+                btUsers.Visibility = Visibility.Visible;
             }
         }
 
@@ -65,45 +49,11 @@ namespace Фасхиева_ПР6
         private void btUsers_Click(object sender, RoutedEventArgs e)
         {
             ClassFrame.frameL.Navigate(new PageAdmin(user));
-        }
+        } 
 
-        private void btUpdateData_Click(object sender, RoutedEventArgs e) //обновление данных
+        private void btnAccount_Click(object sender, RoutedEventArgs e)
         {
-            WindowPersonalAccount windowPerson = new WindowPersonalAccount(user);
-            windowPerson.ShowDialog();
-            ClassFrame.frameL.Navigate(new MenuAdmin(user));
-        }
-
-        private void btnUpdateLogPass_Click(object sender, RoutedEventArgs e) //смена пароля
-        {
-            WindowUpdateLoginPassword windowLogPass = new WindowUpdateLoginPassword(user);
-            windowLogPass.ShowDialog();
-            ClassFrame.frameL.Navigate(new MenuAdmin(user));
-        }
-
-        private void btnPhoto_Click(object sender, RoutedEventArgs e) //добавление фото
-        {
-            try
-            {
-                ClientPhoto p = new ClientPhoto();  // создание объекта для добавления записи в таблицу, где хранится фото
-                p.idClient = user.idClient;
-
-                OpenFileDialog OFD = new OpenFileDialog();  // создаем диалоговое окно                                                       
-                OFD.ShowDialog();  // открываем диалоговое окно             
-                string path = OFD.FileName;  // считываем путь выбранного изображения
-                System.Drawing.Image SDI = System.Drawing.Image.FromFile(path);  // создаем объект для загрузки изображения в базу
-                ImageConverter IC = new ImageConverter();  // создаем конвертер для перевода картинки в двоичный формат
-                byte[] Barray = (byte[])IC.ConvertTo(SDI, typeof(byte[]));  // создаем байтовый массив для хранения картинки
-                p.photoBinary = Barray;  // заполяем поле photoBinary полученным байтовым массивом
-                DataBase.bd.ClientPhoto.Add(p);  // добавляем объект в таблицу БД
-                DataBase.bd.SaveChanges();  // созраняем изменения в БД
-                MessageBox.Show("Успешное добавление фото!");
-                ClassFrame.frameL.Navigate(new MenuAdmin(user)); // перезагружаем страницу
-            }
-            catch
-            {
-                MessageBox.Show("Ошибка с добавлением фотографии!");
-            }
+            ClassFrame.frameL.Navigate(new PageUser(user));
         }
     }
 }
--- Фасхиева_ПР6/Pages/PageUser.xaml.cs
+++ Фасхиева_ПР6/Pages/PageUser.xaml.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -16,6 +17,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using static System.Net.Mime.MediaTypeNames;
+using static System.Net.WebRequestMethods;
 
 namespace Фасхиева_ПР6
 {
@@ -49,22 +51,11 @@ namespace Фасхиева_ПР6
             tbPhone.Text = tbPhone.Text + user.phone;
             tbBirthday.Text = tbBirthday.Text + user.birthday.ToString("dd MMMM yyyy");
 
-            List<ClientPhoto> p = DataBase.bd.ClientPhoto.Where(x => x.idClient == user.idClient).ToList(); // для загрузки картинки находим все фото пользователя в таблице, где хранятся фото
-            if (p.Count != 0)  // если список с фото не пустой, начинает переводить байтовый массив в изображение
-            {
-                byte[] Bar = p[p.Count - 1].photoBinary;   // считываем изображение из базы (считываем байтовый массив двоичных данных) - выбираем последнее добавленное изображение
-                showImage(Bar, imClient);  // отображаем картинку
-            }
-        }
-
-        private void ChoosePhoto_Click(object sender, RoutedEventArgs e) //отображение галлереи
-        {
-            spGallery.Visibility = Visibility.Visible;
             List<ClientPhoto> p = DataBase.bd.ClientPhoto.Where(x => x.idClient == user.idClient).ToList();
-            if (p != null)  // если объект не пустой, начинает переводить байтовый массив в изображение
+            if (p.Count != 0)
             {
-                byte[] Bar = p[n].photoBinary;   // считываем изображение из базы (считываем байтовый массив двоичных данных)
-                showImage(Bar, imGallery);  // отображаем картинку
+                byte[] Bar = p[p.Count - 1].photoBinary;
+                showImage(Bar, imClient);
             }
         }
 
@@ -89,7 +80,6 @@ namespace Фасхиева_ПР6
                 ClientPhoto photo = new ClientPhoto();
                 photo.idClient = user.idClient;
                 OpenFileDialog OFD = new OpenFileDialog();
-                OFD.ShowDialog();
                 string path = OFD.FileName;
                 System.Drawing.Image SDI = System.Drawing.Image.FromFile(path);
                 ImageConverter IC = new ImageConverter();
@@ -105,12 +95,60 @@ namespace Фасхиева_ПР6
                 MessageBox.Show("Ошибка с добавлением фото!");
             }
         }
+        private void ChoosePhoto_Click(object sender, RoutedEventArgs e) //отображение галлереи
+        {
+            MessageBoxResult result = MessageBox.Show("Хотите ли Вы добавить новое фото?", "Обновление фотографии", MessageBoxButton.YesNoCancel);
+            if (result == MessageBoxResult.Yes) //если пользователь хочет изменить фото, то добавляет новое фото
+            {
+                try
+                {
+                    ClientPhoto photo = new ClientPhoto();
+                    photo.idClient = user.idClient;
+                    OpenFileDialog OFD = new OpenFileDialog();
+                    string path = OFD.FileName;
+                    System.Drawing.Image SDI = System.Drawing.Image.FromFile(path);
+                    ImageConverter IC = new ImageConverter();
+                    byte[] Barray = (byte[])IC.ConvertTo(SDI, typeof(byte[]));
+                    photo.photoBinary = Barray;
+                    DataBase.bd.ClientPhoto.Add(photo);
+                    DataBase.bd.SaveChanges();
+                    MessageBox.Show("Успешное добавление фото!");
+                    ClassFrame.frameL.Navigate(new PageUser(user));
+                }
+                catch
+                {
+                    MessageBox.Show("Ошибка с добавлением фото!");
+                }
+            }
+            else if(result == MessageBoxResult.No) //если пользовательне хочет добавлять новое фото, то появляется галерея, из которой можно выбрать фотографию
+            {
+                List<ClientPhoto> p = DataBase.bd.ClientPhoto.Where(x => x.idClient == user.idClient).ToList();
+                if (p.Count != 0)
+                {
+                    UpdatesPhoto_Click();
+                }
+                else
+                {
+                    MessageBox.Show("У пользователя отсутствуют картинки в базе данных!");
+                }
+            }
+        }        
+        void UpdatesPhoto_Click()
+        {
+            spGallery.Visibility = Visibility.Visible;
+            List<ClientPhoto> p = DataBase.bd.ClientPhoto.Where(x => x.idClient == user.idClient).ToList();
+            if (p.Count != 0)  // если объект не пустой, начинает переводить байтовый массив в изображение
+            {
+                byte[] Bar = p[n].photoBinary;   // считываем изображение из базы (считываем байтовый массив двоичных данных)
+                showImage(Bar, imGallery);  // отображаем картинку
+            }
+        }
 
         int n = 0;
         private void btnBack_Click(object sender, RoutedEventArgs e) //кнопка Назад для фотогаллереи
         {
-            List<ClientPhoto> p = DataBase.bd.ClientPhoto.Where(x => x.idClient == user.idClient).ToList();          
-            if (p != null)  // если объект не пустой, начинает переводить байтовый массив в изображение
+            List<ClientPhoto> p = DataBase.bd.ClientPhoto.Where(x => x.idClient == user.idClient).ToList();
+            if (p != null)
             {
                 if (n == 0)
                 {
@@ -121,19 +159,20 @@ namespace Фасхиева_ПР6
                     n = p.Count - 1;
                 }
                 n--;
-                byte[] Bar = p[n].photoBinary;   // считываем изображение из базы (считываем байтовый массив двоичных данных)
-                BitmapImage BI = new BitmapImage();  // создаем объект для загрузки изображения
+                byte[] Bar = p[n].photoBinary;
+                BitmapImage BI = new BitmapImage();
                 showImage(Bar, imGallery);
-            }            
+            }
         }
 
         private void btnNext_Click(object sender, RoutedEventArgs e) //кнопка Вперед для фотогаллереи
         {
             List<ClientPhoto> p = DataBase.bd.ClientPhoto.Where(x => x.idClient == user.idClient).ToList();
             n++;
-            if (p != null)  // если объект не пустой, начинает переводить байтовый массив в изображение
+            if (p != null)
             {
-                byte[] Bar = p[n].photoBinary;   // считываем изображение из базы (считываем байтовый массив двоичных данных)
+
+                byte[] Bar = p[n].photoBinary;
                 showImage(Bar, imGallery);
             }
             if (n == p.Count - 1)
@@ -142,24 +181,24 @@ namespace Фасхиева_ПР6
             }
         }
 
-        private void AddPhotos_Click(object sender, RoutedEventArgs e) //добавление несколькиъ фотографий в базу
+        private void AddPhotos_Click(object sender, RoutedEventArgs e) //добавление нескольких фотографий в базу
         {
             try
             {
-                OpenFileDialog OFD = new OpenFileDialog();  // создаем диалоговое окно
-                OFD.Multiselect = true;  // открытие диалогового окна с возможностью выбора нескольких элементов
-                if (OFD.ShowDialog() == true)  // пока диалоговое окно открыто, будет в цикле записывать каждое выбранное изображение в БД
+                OpenFileDialog OFD = new OpenFileDialog();
+                OFD.Multiselect = true;
+                if (OFD.ShowDialog() == true)
                 {
-                    foreach (string file in OFD.FileNames)  // цикл организован по именам выбранных файлов
+                    foreach (string file in OFD.FileNames)
                     {
-                        ClientPhoto p = new ClientPhoto();  // создание объекта для добавления записи в таблицу, где хранится фото
-                        p.idClient = user.idClient;  // присваиваем значение полю idUser (id авторизованного пользователя)
-                        string path = file;  // считываем путь выбранного изображения
-                        System.Drawing.Image SDI = System.Drawing.Image.FromFile(file);  // создаем объект для загрузки изображения в базу
-                        ImageConverter IC = new ImageConverter();  // создаем конвертер для перевода картинки в двоичный формат
-                        byte[] Barray = (byte[])IC.ConvertTo(SDI, typeof(byte[]));  // создаем байтовый массив для хранения картинки
-                        p.photoBinary = Barray;  // заполяем поле photoBinary полученным байтовым массивом
-                        DataBase.bd.ClientPhoto.Add(p);  // добавляем объект в таблицу БД
+                        ClientPhoto photo = new ClientPhoto();
+                        photo.idClient = user.idClient;
+                        string path = file;
+                        System.Drawing.Image SDI = System.Drawing.Image.FromFile(file);
+                        ImageConverter IC = new ImageConverter();
+                        byte[] Barray = (byte[])IC.ConvertTo(SDI, typeof(byte[]));
+                        photo.photoBinary = Barray;
+                        DataBase.bd.ClientPhoto.Add(photo);
                     }
                     DataBase.bd.SaveChanges();
                     MessageBox.Show("Успешное добавление фотографий!");
@@ -173,9 +212,15 @@ namespace Фасхиева_ПР6
 
         private void btnChoosePhoto_Click(object sender, RoutedEventArgs e) //обновить фотографию на старую
         {
-            List<ClientPhoto> p = DataBase.bd.ClientPhoto.Where(x => x.idClient == user.idClient).ToList();
-            byte[] Bar = p[n].photoBinary;   // считываем изображение из базы (считываем байтовый массив двоичных данных)
-            showImage(Bar, imClient);  // отображаем картинку
+                List<ClientPhoto> p = DataBase.bd.ClientPhoto.Where(x => x.idClient == user.idClient).ToList();
+                byte[] Bar = p[n].photoBinary;
+                showImage(Bar, imClient);
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e) 
+        {
+            ClassFrame.frameL.Navigate(new MenuAdmin(user));     
         }
     }
 }
+
